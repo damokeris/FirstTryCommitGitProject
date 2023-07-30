81c2253d9847166036f4c9ad3bea6285c2d923c9
lipeng <106864183+damokeris@users.noreply.github.com>
2023-07-30 15:15:30 +0800

    学习了Vuex,并做了一个demo,涉及到基本用法

---
+++ src/resources/Vue-Demo/Study-Vuex/Login.vue
@@ -0,0 +1,14 @@
+<template>
+<p>{{ store.getters.getUser }}</p>
+</template>
+
+<script>
+    // ...
+method: {
+        // ...
+        store.dispatch('asyncUpdate', user)
+    }
+</script>
+
+<style>
+</style>
---
+++ src/resources/Vue-Demo/Study-Vuex/store/index.js
@@ -0,0 +1,43 @@
+import { createStore } from 'vuex';
+
+//公共state对象，存储所有组件状态
+const state = {
+    user: {
+        name: ''
+    }
+}
+
+//计算属性
+const getters = {
+    getUser(state) {
+        return state.user
+    }
+}
+
+//修改值的方法，注意是同步阻塞的，调用方式为:store.commit()
+const mutations = {
+    updateUser(state, user) {
+        state.user = user
+    }
+}
+
+//异步调用mutations方法,
+//用dispatch调用actions
+const actions = {
+    asyncUpdate(user) {
+        context.commit('updateUser', user)
+    }
+}
+
+export default Vuex.Store({
+    state,
+    getters,
+    mutations,
+    actions
+})
+
+// export default createStore({
+//     modules:{
+//         User
+//     }
+// })
