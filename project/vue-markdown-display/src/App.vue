eb87c3ac5775675394c7ad6cdd7ab1bda1aa2796
lipeng114514 <1327118851@qq.com>
2023-09-26 08:28:23 +0800

    完成了展示markdown文件的功能

---
+++ project/vue-markdown-display/src/App.vue
@@ -0,0 +1,25 @@
+<!-- 测试markdown转html -->
+<template>
+  <div class="a">v-md-editor预览组件测试：</div>
+  <v-md-preview :text="text"></v-md-preview>
+</template>
+
+<script>
+import markdownContent from './assets/markdown/demo.md?raw';
+
+export default {
+  data() {
+    return {
+      text: markdownContent,
+    };
+  },
+};
+</script>
+
+<style>
+html, body{
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+</style>
--- project/vue-markdown-display/src/main.js
+++ project/vue-markdown-display/src/main.js
@@ -2,4 +2,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import VMdEditor from '@kangc/v-md-editor';
+import VMdPreview from '@kangc/v-md-editor/lib/preview';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
+
+/*// highlightjs
+import hljs from 'highlight.js';
+
+VMdEditor.use(githubTheme, {
+    Hljs: hljs,
+});*/
+VMdPreview.use(vuepressTheme)
+VMdEditor.use(vuepressTheme);
+
+const app = createApp(App)
+
+app.use(VMdEditor);
+app.use(VMdPreview)
+
+app.mount('#app')
